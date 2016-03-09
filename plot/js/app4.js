e6ad6a39211aaba57689e00af15123ac0996d0b6
rwha <rj.whalen@gmail.com>
2016-03-09 09:16:57 -0500

    new stuff

--- plot/js/app4.js
+++ plot/js/app4.js
@@ -17,12 +17,25 @@ curves.Card = function(options) {
 	this.pi = 3.14159265358979323;
 	this.thetamax = 6 * this.pi;
 	this.thetastep = 0.01;
-
+	this.cachedCanvas = (function(t){
+		var c = document.createElement('canvas');
+		c.width = t.width;
+		c.height = t.height;
+		var ctx = c.getContext('2d');
+		ctx.beginPath();
+		ctx.moveTo(0, t.centerY);
+		ctx.lineTo(t.width, t.centerY);
+		ctx.moveTo(t.centerX, 0);
+		ctx.lineTo(t.centerX, t.height);
+		ctx.strokeStyle = '#aaa';
+		ctx.lineWidth = 2;
+		ctx.stroke();
+		return c;
+	}(this));
 }
 
 curves.Card.prototype.parametric = function(point, ctx) {
 	var x,y,xi,yi,tos,los,started = false;
-	ctx.save();
 	ctx.beginPath();
 	for (var s = this.minX; s <= this.maxX; s += this.iteration) {
 		var points = point(s);
@@ -41,12 +54,10 @@ curves.Card.prototype.parametric = function(point, ctx) {
 	}
 	ctx.shadowColor = ctx.strokeStyle = 'green';
 	ctx.stroke();
-	ctx.restore();
 }
 
 curves.Card.prototype.polar = function(radius, ctx) {
 	var r,x,y,xi,yi,tos,los,started=false;
-	ctx.save();
 	ctx.beginPath();
 	for (var theta = 0; theta <= this.thetamax; theta += this.thetastep) {
 		r = radius(theta);
@@ -65,7 +76,6 @@ curves.Card.prototype.polar = function(radius, ctx) {
 	}
 	ctx.shadowColor = ctx.strokeStyle = 'red';
 	ctx.stroke();
-	ctx.restore();
 }
 
 curves.Card.prototype.generateCanvas = function() {
@@ -73,49 +83,46 @@ curves.Card.prototype.generateCanvas = function() {
 	c.width = this.width;
 	c.height = this.height;
 	var ctx = c.getContext('2d');
-	ctx.beginPath();
-	ctx.moveTo(0, this.centerY);
-	ctx.lineTo(this.width, this.centerY);
-	ctx.moveTo(this.centerX, 0);
-	ctx.lineTo(this.centerX, this.height);
-	ctx.strokeStyle = '#aaa';
-	ctx.lineWidth = 2;
-	ctx.stroke();
+	ctx.drawImage(this.cachedCanvas, 0, 0);
 	ctx.lineCap = 'round';
 	ctx.lineJoin = 'round';
 	ctx.shadowBlur = 1;
 	ctx.lineWidth = 2;
-	ctx.save();
 	return c;
 }
 
 curves.Card.prototype.create = function(name) {
+	if(!curves.meta[name]) {
+		console.error('unknown curve: ' + name);
+		return;
+	}
 	var c = this.generateCanvas();
+	c.setAttribute('id', name);
 	var ctx = c.getContext('2d');
-	var curve = curve_meta[name];
-	switch (curve.type) {
+	switch (curves.meta[name].type) {
 		case 'parametric':
-			this.parametric(curve.draw, ctx);
+			this.parametric(curves.meta[name].draw, ctx);
 			break;
 		case 'polar':
-			this.polar(curve.draw, ctx);
+			this.polar(curves.meta[name].draw, ctx);
 			break;
 		default:
-			console.log('unknown function type: ' + curve.type);
+			console.log('unknown function type: ' + curves.meta[name].type);
 	}
+	curves.meta[name].drawn = true;
 	var parent = document.importNode(document.querySelector('#cardTemplate').content, true);
 	parent.querySelector('.canvas').appendChild(c);
-	parent.querySelector('h3').textContent = curve.title;
-	parent.querySelector('span').innerHTML = curve.description;
-	c.style.height = '100%';
+	parent.querySelector('h3').textContent = curves.meta[name].title;
+	parent.querySelector('span').innerHTML = curves.meta[name].description;
 	document.body.appendChild(parent);
 }
 
-var curve_meta = {
+curves.meta = {
 	parabola: {
 		type: "parametric",
 		title: "Parabola",
 		description: "<p>The parabola was studied by Menaechmus who was a pupil of Plato and Eudoxus. He attempted to duplicate the cube, namely to find side of a cube that has a volume double that of a given cube. Hence he attempted to solve x3 = 2 by geometrical methods. In fact the geometrical methods of ruler and compass constructions cannot solve this (but Menaechmus did not know this). Menaechmus solved it by finding the intersection of the two parabolas x2 = y and y2 = 2x.</p><p>Euclid wrote about the parabola and it was given its present name by Apollonius. The focus and directrix of a parabola were considered by Pappus. Pascal considered the parabola as a projection of a circle and Galileo showed that projectiles follow parabolic paths. Gregory and Newton considered the properties of a parabola which bring parallel rays of light to a focus.</p><p>The pedal of the parabola with its vertex as pedal point is a cissoid. The pedal of the parabola with its focus as pedal point is a straight line. With the foot of the directrix as pedal point it is a right strophoid (an oblique strophoid for any other point of the directrix). The pedal curve when the pedal point is the image of the focus in the directrix is a Trisectrix of Maclaurin.</p><p>The evolute of the parabola is Neile's parabola. From a point above the evolute three normals can be drawn to the parabola, while only one normal can be drawn to the parabola from a point below the evolute.</p>",
+		drawn: false,
 		draw: function(s){
 			return {x: s, y: (s * s)};
 		},
@@ -124,6 +131,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Astroid",
 		description: "The astroid was first discussed by Johann Bernoulli in 1691-92. It also appears in Leibniz's correspondence of 1715. It is sometimes called the tetracuspid for the obvious reason that it has four cusps. The astroid only acquired its present name in 1836 in a book published in Vienna. It has been known by various names in the literature, even after 1836, including cubocycloid and paracycle.<br><br>The length of the astroid is 6a and its area is 3&#120587;a2/8. The gradient of the tangent T from the point with parameter p is -tan(p). The equation of this tangent T is:<p class='math'>x sin(p) + y cos(p) = a sin(2p)/2</p>Let T cut the x-axis and the y-axis at X and Y respectively. Then the length XY is a constant and is equal to a. It can be formed by rolling a circle of radius a/4 on the inside of a circle of radius a. It can also be formed as the envelope produced when a line segment is moved with each end on one of a pair of perpendicular axes. It is therefore a glissette.",
+		drawn: false,
 		draw: function(s) {
 			var a = 5;
 			var ci = Math.cos(s);
@@ -137,6 +145,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Bicorn",
 		description: "no description available",
+		drawn: false,
 		draw: function(s) {
 			var a = 5;
 			var sin = Math.sin(s);
@@ -149,6 +158,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Cardiod",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 2;
 			var s = Math.sin(theta);
@@ -161,6 +171,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Cayley's Sextic",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 2;
 			var c = Math.cos(theta/3);
@@ -172,6 +183,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Cissoid of Diocles",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 2;
 			var r = 2 * a * Math.tan(theta) * Math.sin(theta);
@@ -182,6 +194,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Cochleoid",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 9;
 			var r = a * Math.sin(theta)/theta;
@@ -192,6 +205,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Conchoid",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 5;
 			var b = 2;
@@ -203,6 +217,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Conchoid of DeSluze",
 		description: "no description available",
+		drawn: false,
 		draw: function(theta) {
 			var a = 1;
 			var k = 2.5;
@@ -215,6 +230,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Cycloid",
 		description: "no description available",
+		drawn: false,
 		draw: function(s) {
 			var a = 1;
 			var h = 1;
@@ -227,6 +243,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Curate Cycloid",
 		description: "no description available",
+		drawn: false,
 		draw: function(s) {
 			var a = 2;
 			var h = 1;
@@ -239,6 +256,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Prolate Cycloid",
 		description: "no description available",
+		drawn: false,
 		draw: function(s) {
 			var a = 1;
 			var h = 2;
@@ -251,6 +269,7 @@ var curve_meta = {
 		type: "polar",
 		title: "Folium of Descartes",
 		description: "This folium was first discussed by Descartes in 1638 but, although he found the correct shape of the curve in the positive quadrant, he believed that this leaf shape was repeated in each quadrant like the four petals of a flower.<br><br>The problem to determine the tangent to the curve was proposed to Roberval who also wrongly believed the curve had the form of a jasmine flower. His name of fleur de jasmin was later changed.<br><br>The curve is sometimes known as the noeud de ruban. The folium has an asymptote x + y + a = 0.<br><br>The equation of the tangent at the point with t = p is<p class='math'>p(p3 - 2)x + (1 - 2p3)y + 3ap2 = 0</p>The curve passes through the origin at t = 0 and approaches the origin a second time as t goes to infinity.",
+		drawn: false,
 		draw: function(theta) {
 			var a = 3;
 			var s = Math.sin(theta);
@@ -263,6 +282,7 @@ var curve_meta = {
 		type: "parametric",
 		title: "Hypocycloid",
 		description: "no description available",
+		drawn: false,
 		draw: function(s) {
 			var a = 5;
 			var b = 3;
@@ -271,11 +291,25 @@ var curve_meta = {
 			return {x: xi, y: yi};
 		}
 	}
+
 };
 
+/*
+curves.meta.test = {
+	type: "parametric",
+	title: "linear function",
+	description: "slope of one",
+	drawn: false,
+	draw: function(s) {
+		return {x: s, y: s};
+	}
+}
+*/
+
+var cl = Object.getOwnPropertyNames(curves.meta),
+	cards = new curves.Card();
+	
 window.onload = function() {
-	var cl = Object.getOwnPropertyNames(curve_meta);
-	var cards = new curves.Card();
 	for(var i=0,l=cl.length;i<l;i++){
 		cards.create(cl[i]);
 	}
