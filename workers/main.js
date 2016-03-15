48e39871c9ab349c41f2a70bbab6eedb2133edf7
rwha <rj.whalen@gmail.com>
2016-03-15 14:05:47 -0400

    workers stuff.

---
+++ workers/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var worker = new Worker('worker.js');
+
+worker.onmessage = function(e){
+	returned = e.data;
+	
+	if(returned.command === 'list') {
+		console.log(returned.result);
+		var t = document.getElementById('tiles');
+		var frag = document.createDocumentFragment();
+		Object.keys(returned.result).forEach(function(k){
+			var p = document.createElement('div');
+			p.textContent = returned.result[k].title;
+			p.className = 'parent';
+			p.setAttribute('id', k);
+			frag.appendChild(p);
+		});
+		t.appendChild(frag);
+		Array.prototype.forEach.call(t.childNodes, function(n){
+			var nc = n;
+			nc.addEventListener('click', function(e){
+				e.stopPropagation();
+				var s = document.querySelector('.selected');
+				if(s) {
+					s.className = 'parent'; 
+				}
+				this.className = 'selected';
+				//var out = canvas.animate([{opacity: 1},{opacity: 0}], {duration: 200, fill: 'forwards'});
+				//out.onfinish = cards.draw;
+			});
+		});	
+	} else if (returned.command === 'draw') {
+		console.log(returned.result);
+	} else {
+		console.log(returned);
+	}
+}
+
+
+window.onload = function() {
+	worker.postMessage({command: 'list'});
+}
